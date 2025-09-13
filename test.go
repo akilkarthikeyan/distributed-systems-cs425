2a5727d3846912586681b93abbfbe7cc1f2e5cf2
anandan3 <anandan3@fa25-cs425-9501.cs.illinois.edu>
2025-09-13 14:05:10 -0500

    uncomment

--- test.go
+++ test.go
@@ -12,15 +12,15 @@ import (
 // Hosts and their log files
 var hosts = []string{
     "fa25-cs425-9501.cs.illinois.edu",
-    // "fa25-cs425-9502.cs.illinois.edu",
-    // "fa25-cs425-9503.cs.illinois.edu",
-    // "fa25-cs425-9504.cs.illinois.edu",
-    // "fa25-cs425-9505.cs.illinois.edu",
-    // "fa25-cs425-9506.cs.illinois.edu",
-    // "fa25-cs425-9507.cs.illinois.edu",
-    // "fa25-cs425-9508.cs.illinois.edu",
-    // "fa25-cs425-9509.cs.illinois.edu",
-    // "fa25-cs425-9510.cs.illinois.edu",
+    "fa25-cs425-9502.cs.illinois.edu",
+    "fa25-cs425-9503.cs.illinois.edu",
+    "fa25-cs425-9504.cs.illinois.edu",
+    "fa25-cs425-9505.cs.illinois.edu",
+    "fa25-cs425-9506.cs.illinois.edu",
+    "fa25-cs425-9507.cs.illinois.edu",
+    "fa25-cs425-9508.cs.illinois.edu",
+    "fa25-cs425-9509.cs.illinois.edu",
+    "fa25-cs425-9510.cs.illinois.edu",
 }
 
 // Patterns for testing

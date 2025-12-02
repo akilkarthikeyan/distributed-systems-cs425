8283eaa19286724919c6357ae7a70aa12b85d3c0
anandan3 <anandan3@fa25-cs425-9501.cs.illinois.edu>
2025-12-01 18:34:58 -0600

    correct build file

--- mp4/build.sh
+++ mp4/build.sh
@@ -2,4 +2,4 @@
 set -e
 go build -o operators/identity/identity operators/identity/identity.go
 mv operators/identity/identity rainstorm/task/identity
-go build -o rainstorm/task/task rainstorm/task/task.go
+go build -o rainstorm/task/task rainstorm/task/task.go rainstorm/task/types.go
